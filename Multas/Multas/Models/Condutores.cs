b954b54a7032dedc1d8be226bbfb3636fd8fb418
RutePaz <aluno19996@ipt.pt>
2019-04-04 16:55:14 +0100

    Adição do controller Agentes e respetivas views

---
+++ Multas/Multas/Models/Condutores.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models {
+   public class Condutores {
+
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string BI { get; set; }
+
+        public string Telemovel { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string NumCartaConducao { get; set; }
+
+        public string LocalEmissao { get; set; }
+
+        public DateTime DataValidadeCarta { get; set; }
+
+        //identifica as multas associadas ao condutor 
+        public ICollection<Multas> ListaMultas { get; set; }
+    }
+}
---
+++ Multas/Multas/Models/Multas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models {
+   public class Multas {
+
+
+        // id, data, valor, infracao, FK viatura, FK agente, FK condutor
+        public int ID { get; set; }
+
+        public string Infracao { get; set; }
+
+        public string LocalDaMulta { get; set; }
+
+        public decimal ValorMulta { get; set; }
+
+        public DateTime DataDaMulta { get; set; }
+
+        //Criar chaves forasteiras
+        //para a base de dados 
+        [ForeignKey("Agente")]
+        public int AgenteFK { get; set; }
+        //para o C#
+        public Agentes Agente { get; set; }
+
+        //FK para o Condutor
+        //para a base de dados 
+        [ForeignKey("Condutor")]
+        public int CondutorFK { get; set; }
+        //para o C#
+        public Condutores Condutor { get; set; }
+
+        //FK para a Viatura
+        //para a base de dados 
+        [ForeignKey("Viatura")]
+        public int ViaturaFK { get; set; }
+        //para o C#
+        public Viaturas Viatura{ get; set; }
+
+
+
+
+
+
+
+    }
+}
