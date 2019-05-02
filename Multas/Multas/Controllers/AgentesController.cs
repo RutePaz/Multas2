ffe2f5e944d6a30daeb54ade6bc5dda499c81986
RutePaz <aluno19996@ipt.pt>
2019-05-02 17:08:50 +0100

    Formatação dos parâmetros de proteção de dados no Controller

--- Multas/Multas/Controllers/AgentesController.cs
+++ Multas/Multas/Controllers/AgentesController.cs
@@ -12,25 +12,43 @@ namespace Multas.Controllers
 {
     public class AgentesController : Controller
     {
+        //cria uma variavel que representa a base de dados
         private MultasDB db = new MultasDB();
 
         // GET: Agentes
         public ActionResult Index()
         {
-            return View(db.Agentes.ToList());
+            //procura a totalidade dos agente na base de dados 
+            //Intrução feita em LINQ
+            //SELECT * from Agentes ORDER BY name
+            var listaAgentes = db.Agentes.OrderBy(a=>a.Nome).ToList();
+            return View(listaAgentes);
         }
 
         // GET: Agentes/Details/5
+        /// <summary>
+        /// Mostra os dados de um Agente 
+        /// </summary>
+        /// <param name="id">identifica o Agente</param>
+        /// <returns>Devolve a View com os dados</returns>
+        /// int? - pode nao ser preenchido
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                //resposta por defeito
+                //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                //este erro ocorre pois o utilizador anda a "fazer asneiras"
+                return RedirectToAction("Index");
+
             }
+            //SELECT * FROM Agentes WHERE ID=id
             Agentes agentes = db.Agentes.Find(id);
+            //o Agente foi encontrado?
             if (agentes == null)
             {
-                return HttpNotFound();
+                //O Ãgente não foi encontrado, porque o utilizador está 'à pesca'
+                return RedirectToAction("Index");
             }
             return View(agentes);
         }
@@ -45,17 +63,21 @@ namespace Multas.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        //anotação para proteger o código de ataques 
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agentes)
+        public ActionResult Create([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agente)
         {
             if (ModelState.IsValid)
             {
-                db.Agentes.Add(agentes);
+                //adição de um novo agente
+                db.Agentes.Add(agente);
+                //"commit" 
                 db.SaveChanges();
+                //devolve o controlo para a página do index
                 return RedirectToAction("Index");
             }
-
-            return View(agentes);
+            //caso nao seja válido
+            return View(agente);
         }
 
         // GET: Agentes/Edit/5
@@ -63,14 +85,14 @@ namespace Multas.Controllers
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return RedirectToAction("Index");
             }
-            Agentes agentes = db.Agentes.Find(id);
-            if (agentes == null)
+            Agentes agente = db.Agentes.Find(id);
+            if (agente == null)
             {
-                return HttpNotFound();
+                return RedirectToAction("Index");
             }
-            return View(agentes);
+            return View(agente);
         }
 
         // POST: Agentes/Edit/5
@@ -78,15 +100,15 @@ namespace Multas.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agentes)
+        public ActionResult Edit([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agente)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(agentes).State = EntityState.Modified;
+                db.Entry(agente).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(agentes);
+            return View(agente);
         }
 
         // GET: Agentes/Delete/5
@@ -94,24 +116,68 @@ namespace Multas.Controllers
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return RedirectToAction("Index");
             }
-            Agentes agentes = db.Agentes.Find(id);
-            if (agentes == null)
+            Agentes agente = db.Agentes.Find(id);
+            if (agente == null)
             {
-                return HttpNotFound();
+                return RedirectToAction("Index");
             }
-            return View(agentes);
+            //o agente foi encontrado 
+            //vou salvaguardar os dados para posterior validação
+            //- guardar o ID do Agente num cookie cifrado - vai para o cliente 
+            //- guardar o ID numa variável de sessão - uma variável de sessão nunca vai para o cliente fica sempre no servidor
+            //    (se se usar o ASP .NET Core, esta ferramenta já não existe
+            //- outras alternativas válidas 
+            Session["Agente"] = agente.ID;
+            //mostra na view os dados do agente
+            return View(agente);
         }
 
         // POST: Agentes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult DeleteConfirmed(int? id)
         {
-            Agentes agentes = db.Agentes.Find(id);
-            db.Agentes.Remove(agentes);
-            db.SaveChanges();
+            if (id == null) {
+                return RedirectToAction("Index");
+            }
+            //o ID não é nulo 
+            //Será o ID o que eu espero?
+            //vamos validar se o ID está correto
+            if (id != (int) Session["Agente"]) {
+                return RedirectToAction("Index");
+            }
+
+
+
+            //procura o agente a remover
+            Agentes agente = db.Agentes.Find(id);
+
+            if (agente == null) {
+                //não foi encontrado o agente 
+                return RedirectToAction("Index");
+            }
+            try
+            {
+               
+                //dá ordem de remoção do agente
+                db.Agentes.Remove(agente);
+                //consolida a remoção
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                //devem aqui ser escritas todas as instruções que são consideradas necessárias 
+
+                //informar que houve um erro 
+                ModelState.AddModelError("", "Não é possível remover o Agente. Provavelmente, o  agente, "+ agente.Nome +", tem multas associadas a ele...");
+
+                //redirecionar para a página onde o erro foi gerado
+                return View(agente);
+            }
+            
+
             return RedirectToAction("Index");
         }
 
--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,11 +11,31 @@ namespace Multas.Models {
 
 
         public int ID { get; set; }
+        [Required(ErrorMessage = "Por favor, insira o nome do agente.")]
+        //validar um nome
+        //letras, formando palavras
+        //letras isoladas, em alguns casos. ex:e, de, do, dos, etc.
+        //espaço em branco entre palavras 
+        //uso de simbolos especiais. ex: letras acedelhadas, -, '
+        // \d{4}-\{3}\w* - indicação para o código postal
+        //[]- identifica conjunto
+        //() - agrupamentos, juntam um conjunto de regras
+        //+ - quanrificador : 1 ou mais
+        //* - quantificador : 1  ou mais
+        //? -quantufucador: 0 ou 1
+        //()+ o agrupamento ira acontecer um ou mais vezes 
+        //()* o agrupamento ira acontecer as vezes que o utilizador desejar
+        //{i} quantificador: indica que são exatamente i valores 
+        //{i,j} quantificador: indica que são exatamente entre i e j valores
+        // | - seprara opções de escolha
+        [RegularExpression("([A-ZÁÉÍÓÚa-záéíóúàèìòùãõâêîôûäëïöüçñ]+( |-|')?)+", ErrorMessage = "Só pode escrever letras no Nome. Deve começar por uma maiúcula ")]
 
+        //(((((e|de|da|do|das|dos) )?)|-|')+[A-ZÁÉÍÓÚ][a-záéíóúàèìòùãõâêîôûäëïöüçñ]+){2,3}
         public string Nome { get; set; }
-
+        [Required(ErrorMessage = "Por favor, insira a esquadra onde o agente trabalha.")]
+       // [RegularExpression ("Tomar|Ourém|Lisboa|Leiria|Alcanena|Torres Novas")]
         public string Esquadra { get; set; }
-
+        
         public string Fotografia { get; set; }
 
         //identifica as multas passadas pelo Agente
